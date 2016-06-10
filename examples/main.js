207abede6bf667e90fdb4b62cc80eaef50bd0475
Maciej Korzepa <s151350@student.dtu.dk>
2016-06-10 10:07:23 +0200

    feat: legend integrated with lineages

--- examples/main.js
+++ examples/main.js
@@ -248,12 +248,6 @@ function createRandomLineageScatterPlotData2(totalNodes, n) {
 let data = {
     data: nodesArr,
     layout: {
-        xAxis: {
-            title: "xAxis"
-        },
-        yAxis: {
-            title: "yAxis"
-        },
         nodeTypes: {
             "type1": {
                 r: 4,
@@ -273,6 +267,18 @@ let data = {
         },
         heatmap: {
             enabled: true
+        },
+        legend: {
+            show: true,
+            position: {
+                "x": "center",
+                "y": "top"
+            },
+            anchor: {
+                "x": "inside",
+                "y": "inside"
+            },
+            orientation: "horizontal"
         }
     }
 };
@@ -352,6 +358,18 @@ let data5 = {
             colourBar: {
                 show: true
             }
+        },
+        legend: {
+            show: true,
+            position: {
+                "x": "right",
+                "y": "center"
+            },
+            anchor: {
+                "x": "outside",
+                "y": "inside"
+            },
+            orientation: "vertical"
         }
     }
 };
@@ -391,8 +409,20 @@ for (let s = 0; s < data4.series.length; s++) {
     }
 }
 
-let phylo = JSON.parse('{"data":[{"taxon":null,"length":null,"children":[{"taxon":null,"length":1.375,"children":[{"taxon":{"name":"H1","z":1,"parent":null,"series":0,"type":"pool"},"length":1,"children":[]},{"taxon":null,"length":0,"children":[{"taxon":{"name":"H2","parent":null,"z":2,"series":0,"type":"pool"},"length":1,"children":[]},{"taxon":null,"length":0,"children":[{"taxon":{"name":"H3","z":null,"parent":null,"series":0,"type":"pool"},"length":1,"children":[]},{"taxon":null,"length":1,"children":[{"taxon":{"name":"H4","parent":null,"z":4,"series":0,"type":"pool"},"length":0,"children":[]},{"taxon":{"name":"H55","parent":null,"z":3.2,"series":0,"type":"pool"},"length":0,"children":[]}]}]}]}]},{"taxon":null,"length":1.375,"children":[{"taxon":null,"length":2.75,"children":[{"taxon":null,"length":0.5833333333333334,"children":[{"taxon":{"name":"H5","series":0,"z":0,"type":"pool"},"length":0.2857142857142857,"children":[]},{"taxon":{"name":"H6","z":null,"parent":null,"series":0,"type":"pool"},"length":1.7142857142857144,"children":[]}]},{"taxon":{"name":"H8","z":null,"parent":null,"series":0,"type":"pool"},"length":0.41666666666666663,"children":[]}]},{"taxon":{"name":"H7","z":null,"parent":null,"series":0,"type":"pool"},"length":0.25,"children":[]}]}]}],"layout":{"nodeTypes":{"strain":{"r":4,"strokeWidth":3},"pool":{"r":6,"strokeWidth":1}},"groupSelection":{"enabled":true,"selectionRectangle":{"stroke-width":1,"stroke-dasharray":4,"rx":3,"ry":3,"stroke":"steelblue"}},"labelCollisionDetection":{"enabled":"onDelay","updateDelay":500},"showLeafNodes":true,"maxZoom":50,"title":"","size":600,"axis":{"title":"","show":true,"gridOnly":false,"valueProperty":"default"},"nodeLabel":{"font-size":12},"heatmap":{"enabled":true,"colourScale":[[0,"#008ae5"],[1,"yellow"]],"colourBar":{"show":true,"height":"70%","width":30},"circle":{"r":16},"opacity":0.4}}}');
+let phylo = JSON.parse('{"data":[{"taxon":null,"length":null,"children":[{"taxon":null,"length":1.375,"children":[{"taxon":{"name":"H1","z":1,"parent":null,"series":0,"type":"pool"},"length":1,"children":[]},{"taxon":null,"length":0,"children":[{"taxon":{"name":"H2","parent":null,"z":2,"series":0,"type":"pool"},"length":1,"children":[]},{"taxon":null,"length":0,"children":[{"taxon":{"name":"H3","z":null,"parent":null,"series":1,"type":"pool"},"length":1,"children":[]},{"taxon":null,"length":1,"children":[{"taxon":{"name":"H4","parent":null,"z":4,"series":0,"type":"pool"},"length":0,"children":[]},{"taxon":{"name":"H55","parent":null,"z":3.2,"series":1,"type":"pool"},"length":0,"children":[]}]}]}]}]},{"taxon":null,"length":1.375,"children":[{"taxon":null,"length":2.75,"children":[{"taxon":null,"length":0.5833333333333334,"children":[{"taxon":{"name":"H5","series":1,"z":0,"type":"pool"},"length":0.2857142857142857,"children":[]},{"taxon":{"name":"H6","z":null,"parent":null,"series":0,"type":"pool"},"length":1.7142857142857144,"children":[]}]},{"taxon":{"name":"H8","z":null,"parent":null,"series":0,"type":"pool"},"length":0.41666666666666663,"children":[]}]},{"taxon":{"name":"H7","z":null,"parent":null,"series":0,"type":"pool"},"length":0.25,"children":[]}]}]}],"layout":{"nodeTypes":{"strain":{"r":4,"strokeWidth":3},"pool":{"r":6,"strokeWidth":1}},"groupSelection":{"enabled":true,"selectionRectangle":{"stroke-width":1,"stroke-dasharray":4,"rx":3,"ry":3,"stroke":"steelblue"}},"labelCollisionDetection":{"enabled":"onDelay","updateDelay":500},"showLeafNodes":true,"maxZoom":50,"title":"","size":600,"axis":{"title":"","show":true,"gridOnly":false,"valueProperty":"default"},"nodeLabel":{"font-size":12},"heatmap":{"enabled":true,"colourScale":[[0,"#008ae5"],[1,"yellow"]],"colourBar":{"show":true,"height":"70%","width":30},"circle":{"r":16},"opacity":0.4}}}');
 phylo.data.push(JSON.parse(JSON.stringify(phylo.data[0])));
+phylo.layout.legend = {
+    show: false,
+    position: {
+        "x": "center",
+        "y": "bottom"
+    },
+    anchor: {
+        "x": "inside",
+        "y": "outside"
+    },
+    orientation: "horizontal"
+};
 
 class AppController {
     constructor($scope, $http) {
@@ -409,7 +439,7 @@ class AppController {
         $scope.boxPlotData = data3;
         $scope.violinPlotData = data3;
         $scope.timeData = data4;
-        $scope.showBranchLengths = true;
+        $scope.showBranchLengths = false;
         $scope.phyloData = phylo;
 
     }
